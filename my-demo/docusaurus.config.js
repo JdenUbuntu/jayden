6dae9a375be06ac055723c84873a08efec132d4f
JdenUbuntu <jayden060802@gmail.com>
2024-12-02 13:41:03 +0800

    8th

--- my-demo/docusaurus.config.js
+++ my-demo/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'JdenUbuntu', // Usually your GitHub org/user name.
-  projectName: 'jayden', // Usually your repo name.
+  projectName: 'my-demo', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
